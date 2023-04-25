78b41e1116b045214daf44f39e3733586c9e73aa
Hao Zhang <hzhangxyz@outlook.com>
2023-04-25 13:25:10 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "rt", encoding="utf-8") as file:
+    long_description = file.read()
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name="gpt_cli_bot",
+    version="0.0.1",
+    description="GPT Commandline interface bot",
+    author="Hao Zhang",
+    author_email="zh970205@mail.ustc.edu.cn",
+    url="https://github.com/hzhangxyz/gpt-cli-bot",
+    packages=find_packages(),
+    license="GPLv3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    python_requires=">=3.9",
+)
