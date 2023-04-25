3514ec1b01de78d8d2046191b81dae787625a184
Hao Zhang <hzhangxyz@outlook.com>
2023-04-25 13:37:44 +0800

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="gpt_bot",
-    version="0.0.1",
+    version="0.0.2",
     description="GPT Commandline interface bot",
     author="Hao Zhang",
     author_email="zh970205@mail.ustc.edu.cn",
